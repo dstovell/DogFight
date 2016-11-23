2ab942110ef6b8b145704d1d61112764c33b486b
dstovell <ninja@Daniels-iMac.local>
2016-11-23 03:03:05 -0800

    Ship Controller v0.1

---
+++ DogFight/Arena.cs
@@ -0,0 +1,112 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using Pathfinding;
+
+namespace DogFight
+{
+
+public class Arena : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () 
+	{
+		GenerateArena();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+	
+	}
+
+	public float ArenaRadius = 50.0f;
+	public float ArenaLength = 100.0f;
+	public float ArenaCapLength = 20.0f;
+
+	public int ArenaLayers = 2;
+	public int ArenaArcSegments = 6;
+	public int ArenaSegments = 50;
+
+	public GameObject EndA;
+	public GameObject EndB;
+
+	private GameObject NavPoints;
+
+	private GameObject AddNavPoint(Vector3 pos, Transform parent)
+	{
+		GameObject obj = new GameObject("NavPoint");
+		obj.tag = "NavPoint";
+		obj.transform.position = pos;
+		obj.transform.SetParent(parent);
+		return obj;
+	}
+
+	private void GenerateSegment(Vector3 centerPos, float radius)
+	{
+		float arcSize = (2*Mathf.PI)/this.ArenaArcSegments;
+		for (int i=0; i<this.ArenaArcSegments; i++)
+		{
+			float angle = (float)i * arcSize;
+			float x = radius * Mathf.Cos(angle);
+			float y = radius * Mathf.Sin(angle);
+			Vector3 pos = centerPos + new Vector3(x, y);
+			this.AddNavPoint(pos, this.NavPoints.transform);
+		}
+
+		this.AddNavPoint(centerPos, this.NavPoints.transform);
+	}
+
+	public void GenerateArena()
+	{
+		this.NavPoints = new GameObject("NavPoints");
+		this.NavPoints.transform.SetParent(this.transform);
+		float segmentLength = this.ArenaLength / (float)this.ArenaSegments;
+		float layerMultiple = this.ArenaRadius / (float)this.ArenaLayers;
+		for (int i=0; i<this.ArenaLayers; i++)
+		{
+			float radius = this.ArenaRadius - (float)i * layerMultiple;
+			for (int j=0; j<this.ArenaSegments; j++)
+			{
+				Vector3 pos = new Vector3(0.0f, 0.0f, (float)j * segmentLength);
+				this.GenerateSegment(pos, radius);
+			}
+		}
+
+		this.EndA = this.AddNavPoint(new Vector3(0.0f, 0.0f, -1.0f*this.ArenaCapLength), this.transform);
+		this.EndA.name = "EndA";
+		this.EndB = this.AddNavPoint(new Vector3(0.0f, 0.0f, this.ArenaLength + this.ArenaCapLength), this.transform);
+		this.EndB.name = "EndB";
+
+		AstarPath.active.astarData.pointGraph.limits = new Vector3(layerMultiple, layerMultiple, 1.5f*segmentLength);
+		AstarPath.active.astarData.pointGraph.maxDistance = 1.5f*segmentLength;
+		AstarPath.active.Scan();
+
+		AstarPath.RegisterSafeUpdate (() => {
+			PointGraph graph = AstarPath.active.astarData.pointGraph;
+			for (int i=0; i<graph.nodes.Length; i++)
+			{
+				PointNode node = graph.nodes[i];
+				if (node != null)
+				{
+					List<GraphNode> connsToRemove = new List<GraphNode>();
+					for (int j=0; j<node.connections.Length; j++)
+					{
+						GraphNode n = node.connections[j];
+						if (n.position.z == node.position.z)
+						{
+							connsToRemove.Add(n);
+						}
+					}
+
+					for (int j=0; j<connsToRemove.Count; j++)
+					{
+						node.RemoveConnection(connsToRemove[j]);
+					}
+				}
+			}
+		});
+	}
+}
+
+}
---
+++ DogFight/ShipController.cs
@@ -0,0 +1,92 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using DSTools;
+
+namespace DogFight
+{
+
+public class ShipController : MessengerListener
+{
+	public float MoveSpeed = 1f;
+	public float RotateSpeed = 1f;
+
+	private Rigidbody rb;
+
+	public SWS.PathManager currentPath;
+
+	public Quaternion desiredRotation;
+
+	private SWS.splineMove mover;
+	private SWS.PathManager pathManager;
+
+	private CapsuleCollider capsule;
+
+	// Use this for initialization
+	void Start() 
+	{
+		this.InitMessenger("ShipController");
+		this.rb = this.gameObject.GetComponent<Rigidbody>();
+		this.mover = this.gameObject.GetComponent<SWS.splineMove>();
+		this.pathManager = this.gameObject.GetComponent<SWS.PathManager>();
+
+		//this.mover.lockRotation = DG.Tweening.AxisConstraint.X;
+	}
+
+	void Update () 
+	{
+		
+	}
+
+	public bool IsFacing(Vector3 dir)
+	{
+		return (Vector3.Angle(this.transform.forward, dir) < 0.1);
+	}
+
+	public bool IsMoving()
+	{
+		return (this.currentPath != null);
+	}
+
+	public SWS.PathManager GeneratePathManager(string name, List<Transform> points)
+	{
+		GameObject obj = new GameObject();
+		obj.name = name + "Path";
+
+		SWS.PathManager path = obj.AddComponent<SWS.PathManager>();
+		path.waypoints = new Transform[points.Count];
+
+		for (int i=0; i<points.Count; i++)
+		{
+			points[i].SetParent(obj.transform);
+			path.waypoints[i] = points[i];
+		}
+
+		return path;
+	}
+
+	public void MovePath(List<Transform> points)
+	{
+		this.currentPath = this.GeneratePathManager("PlayerRow", points);
+	}
+
+	public void Stop()
+	{
+		if (this.mover != null)
+		{
+			this.mover.Stop();
+		}
+		if (this.currentPath != null)
+		{
+			GameObject.Destroy(this.currentPath.gameObject);
+			this.currentPath = null;
+		}
+	}
+
+	void OnCollisionEnter(Collision info)
+	{
+		//GameObject obj = info.collider.gameObject;
+	}
+}
+
+}
