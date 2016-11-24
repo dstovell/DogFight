341da4b55a5f1c48b1f0d55a6a4aea92a3f0df42
dstovell <ninja@Daniels-iMac.local>
2016-11-24 01:07:45 -0800

    Ship Leader for better flying

--- DogFight/ShipController.cs
+++ DogFight/ShipController.cs
@@ -10,32 +10,38 @@ public class ShipController : MessengerListener
 {
 	public float MoveSpeed = 1f;
 	public float RotateSpeed = 1f;
+	public float DesiredLeaderDist = 5f;
+
+	public ShipLeader Leader;
 
 	private Rigidbody rb;
 
-	public SWS.PathManager currentPath;
-
-	public Quaternion desiredRotation;
-
-	private SWS.splineMove mover;
-	private SWS.PathManager pathManager;
-
-	private CapsuleCollider capsule;
+	private float currentSpeed = 0f;
 
 	// Use this for initialization
 	void Start() 
 	{
 		this.InitMessenger("ShipController");
 		this.rb = this.gameObject.GetComponent<Rigidbody>();
-		this.mover = this.gameObject.GetComponent<SWS.splineMove>();
-		this.pathManager = this.gameObject.GetComponent<SWS.PathManager>();
 
-		//this.mover.lockRotation = DG.Tweening.AxisConstraint.X;
+		this.Leader.gameObject.transform.SetParent(null);
 	}
 
-	void Update () 
+	void Update() 
 	{
-		
+		Vector3 desiredDirection = (this.Leader.transform.position - this.transform.position).normalized;
+		Quaternion desiredRotation = Quaternion.LookRotation(desiredDirection);
+		this.transform.rotation = Quaternion.RotateTowards(this.transform.rotation, desiredRotation, this.RotateSpeed*Time.deltaTime);
+
+		float distanceToLeader = Vector3.Distance(this.transform.position, this.Leader.transform.position);
+		this.currentSpeed = this.MoveSpeed * Mathf.Min(distanceToLeader/this.DesiredLeaderDist, 2.0f);
+		this.transform.position += this.transform.forward * this.currentSpeed * Time.deltaTime;
+
+		UpdateThrusters();
+	}
+
+	private void UpdateThrusters()
+	{
 	}
 
 	public bool IsFacing(Vector3 dir)
@@ -43,44 +49,24 @@ public class ShipController : MessengerListener
 		return (Vector3.Angle(this.transform.forward, dir) < 0.1);
 	}
 
-	public bool IsMoving()
+	public float GetSpeed()
 	{
-		return (this.currentPath != null);
+		return currentSpeed;
 	}
 
-	public SWS.PathManager GeneratePathManager(string name, List<Transform> points)
+	public bool IsMoving()
 	{
-		GameObject obj = new GameObject();
-		obj.name = name + "Path";
-
-		SWS.PathManager path = obj.AddComponent<SWS.PathManager>();
-		path.waypoints = new Transform[points.Count];
-
-		for (int i=0; i<points.Count; i++)
-		{
-			points[i].SetParent(obj.transform);
-			path.waypoints[i] = points[i];
-		}
-
-		return path;
+		return this.Leader.IsMoving();
 	}
 
 	public void MovePath(List<Transform> points)
 	{
-		this.currentPath = this.GeneratePathManager("PlayerRow", points);
+		this.Leader.MovePath(points);
 	}
 
 	public void Stop()
 	{
-		if (this.mover != null)
-		{
-			this.mover.Stop();
-		}
-		if (this.currentPath != null)
-		{
-			GameObject.Destroy(this.currentPath.gameObject);
-			this.currentPath = null;
-		}
+		this.Leader.Stop();
 	}
 
 	void OnCollisionEnter(Collision info)
---
+++ DogFight/ShipLeader.cs
@@ -0,0 +1,71 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using Pathfinding;
+
+namespace DogFight
+{
+
+public class ShipLeader : MonoBehaviour
+{
+
+	public SWS.PathManager currentPath;
+
+	public SWS.splineMove mover;
+	public SWS.PathManager pathManager;
+
+	// Use this for initialization
+	void Start ()
+	{
+		this.mover = this.gameObject.GetComponent<SWS.splineMove>();
+		this.pathManager = this.gameObject.GetComponent<SWS.PathManager>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	
+	}
+
+	private SWS.PathManager GeneratePathManager(string name, List<Transform> points)
+	{
+		GameObject obj = new GameObject();
+		obj.name = name + "Path";
+
+		SWS.PathManager path = obj.AddComponent<SWS.PathManager>();
+		path.waypoints = new Transform[points.Count];
+
+		for (int i=0; i<points.Count; i++)
+		{
+			points[i].SetParent(obj.transform);
+			path.waypoints[i] = points[i];
+		}
+
+		return path;
+	}
+
+	public void MovePath(List<Transform> points)
+	{
+		this.currentPath = this.GeneratePathManager("Ship", points);
+	}
+
+	public bool IsMoving()
+	{
+		return (this.currentPath != null);
+	}
+
+	public void Stop()
+	{
+		if (this.mover != null)
+		{
+			this.mover.Stop();
+		}
+		if (this.currentPath != null)
+		{
+			GameObject.Destroy(this.currentPath.gameObject);
+			this.currentPath = null;
+		}
+	}
+}
+
+}
