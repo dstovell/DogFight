cc1b094f22fc0907040ad37c4a0d91874d4ee527
dstovell <ninja@Daniels-iMac.local>
2017-01-08 03:18:50 -0800

    ChangeSplineGroupConditionalAction

---
+++ DogFight/ConditionalActions/ChangeSplineGroupConditonalAction.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace DogFight
+{
+
+public class ChangeSplineGroupConditonalAction : DSTools.ConditionalAction
+{
+	public SplineGroup From;
+	public SplineGroup To;
+
+	public ShipController [] Ships;
+
+	protected override bool OnConditionsMet(GameObject triggerer)
+	{
+		for (int i=0; i<this.Ships.Length; i++)
+		{
+			ShipController ship = this.Ships[i];
+			if ((ship != null) && (ship.gameObject == triggerer))
+			{
+				SplineGroup currentGroup = ship.GetSplineGroup();
+				if ((this.From == null) || (currentGroup == null) || (this.From == currentGroup))
+				{
+					if (currentGroup != this.To)
+					{
+						ship.SetSplineGroup(this.To);
+						return true;
+					}
+				}
+			}
+		}
+		return false;
+	}
+}
+
+}
