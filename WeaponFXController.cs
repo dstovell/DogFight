a3a996957560aa6a4e307d4a428f5f72b0965c05
dstovell <ninja@Daniels-iMac.local>
2017-01-16 01:24:38 -0800

    Get Weapon damage working

--- WeaponFXController.cs
+++ WeaponFXController.cs
@@ -1,8 +1,19 @@
 ﻿using UnityEngine;
 using System.Collections;
+using Forge3D;
 
-public class WeaponFXController : Forge3D.F3DFXController
+public class WeaponFXController : F3DFXController
 {
+	public override void OnProjectileHit(F3DProjectile projectile, GameObject obj, Vector3 hitPoint)
+    {
+		DogFight.Damageable damageable = obj.GetComponent<DogFight.Damageable>();
+		Debug.LogError("WeaponFXController OnProjectileHit name=" + obj.name + " rootName=" + obj.transform.root.gameObject.name + " damageable=" + damageable);
+		if (damageable != null)
+		{
+			damageable.Damage(20f);
+		}
+    }
+
 	void OnGUI()
 	{
 	}
